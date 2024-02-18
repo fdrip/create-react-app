246fd1ad0728fcca044cf4efbb7f7cd07bc865d7
fdrip <dev.feetdp@outlook.com>
2024-02-18 13:58:44 -0500

    firstcommit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleQuit = () => {
+    const newWindow = window.open("", "_self");
+    window.close();
+    newWindow.close();
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thank you for subscribing, you can cancel your subscription at anytime in your account settings.
+          <div>
+            <button onClick={handleQuit}> Quit </button>
+          </div>
         </p>
         <a
           className="App-link"
